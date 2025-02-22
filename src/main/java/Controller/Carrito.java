36f2570d8ec3f7886e433d28a108aac510bb3c72
Yeison Berbesi Chapeta <yeison.berbesi@gmail.com>
2025-02-21 21:57:32 -0500

    Se agrega logica clase Carrito

--- src/main/java/Controller/Carrito.java
+++ src/main/java/Controller/Carrito.java
@@ -1,4 +1,35 @@
 package Controller;
 
 public class Carrito {
+
+    private static ArrayList<Producto> productos;
+
+    public Carrito() {
+        this.productos = new ArrayList<>();
+    }
+
+    // metodo para agregar los productos al carrito de compras
+    public static void agregarProducto(Producto producto) {
+        productos.add(producto);
+    }
+
+    // metodo para eliminar los productos del carrito de compras
+    public void eliminarProducto(int id) {
+        for (Producto producto : productos) {
+            if (producto.getID() == id) {
+                productos.remove(producto);
+                System.out.println("Producto eliminado con exito.");
+                return;
+            }
+        }
+        System.out.println("Producto no encontrado en el carro de compra.");
+    }
+
+    // metodo para mostrar los productos del carrito de compras
+    public static void mostrarCarro() {
+        System.out.println("----- Carro de compras -----");
+        for (Producto producto : productos) {
+            System.out.println(producto.toString());
+        }
+    }
 }
